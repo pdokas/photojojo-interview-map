326968a976cc2cf6ef57f37d734fe1bd1b05cad8
Phil Dokas <phil@jetless.org>
2011-07-17 16:35:19 -0400

    Stubbing order data

--- index.php
+++ index.php
@@ -1,3 +1,42 @@
+<?php
+
+/* Stub Data */
+
+$orders = array(
+	array(
+		'id'       => 'NDP-CMIFS',
+		'name'     => 'Bart Simpson',
+		'address'  => '1243 Marlborough, Ann Arbor, MI 48104',
+		'tracking' => '9101901000704027882045',
+		'geocode'  => array(
+			'lat'  => 42.25184,
+			'lng'  => -83.73086
+		)
+	),
+	array(
+		'id'       => 'NDP-FNORD',
+		'name'     => 'Fox Mulder',
+		'address'  => '201 N Main St., Roswell, NM 88201',
+		'tracking' => '1ZE587150331792478',
+		'geocode'  => array(
+			'lat'  => 33.39440,
+			'lng'  => -104.52262
+		)
+	),
+	array(
+		'id'       => 'NDP-NARF',
+		'name'     => 'Baron Von Savings',
+		'address'  => '398 Lucky St., Truth or Consequences, NM 87901',
+		'tracking' => 'LF002345052US',
+		'geocode'  => array(
+			'lat'  => 33.13417,
+			'lng'  => -107.23851
+		)
+	)
+);
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,6 +44,6 @@
 </head>
 
 <body>
-	Foo
+	
 </body>
 </html>
