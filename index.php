b93ce9aa9188f5af78d3876b1886edcce5aca7db
Phil Dokas <phil@jetless.org>
2011-07-17 18:59:16 -0400

    Refactoring out in lib file

--- index.php
+++ index.php
@@ -1,74 +1,47 @@
 <?php
 
-/* Stub Data */
+	require('lib.php');
 
-$orders = array(
-	array(
-		'id'       => 'NDP-CMIFS',
-		'name'     => 'Bart Simpson',
-		'address'  => '1243 Marlborough, Ann Arbor, MI 48104',
-		'tracking' => '9101901000704027882045',
-		'geocode'  => array(
-			'lat'  => 42.25184,
-			'lng'  => -83.73086
+	/* Stub Data */
+	$orders = array(
+		array(
+			'id'       => 'NDP-CMIFS',
+			'name'     => 'Bart Simpson',
+			'address'  => '1243 Marlborough, Ann Arbor, MI 48104',
+			'tracking' => '9101901000704027882045',
+			'geocode'  => array(
+				'lat'  => 42.25184,
+				'lng'  => -83.73086
+			)
+		),
+		array(
+			'id'       => 'NDP-FNORD',
+			'name'     => 'Fox Mulder',
+			'address'  => '201 N Main St., Roswell, NM 88201',
+			'tracking' => '1ZE587150331792478',
+			'geocode'  => array(
+				'lat'  => 33.39440,
+				'lng'  => -104.52262
+			)
+		),
+		array(
+			'id'       => 'NDP-NARF',
+			'name'     => 'Baron Von Savings',
+			'address'  => '398 Lucky St., Truth or Consequences, NM 87901',
+			'tracking' => 'LF002345052US',
+			'geocode'  => array(
+				'lat'  => 33.13417,
+				'lng'  => -107.23851
+			)
 		)
-	),
-	array(
-		'id'       => 'NDP-FNORD',
-		'name'     => 'Fox Mulder',
-		'address'  => '201 N Main St., Roswell, NM 88201',
-		'tracking' => '1ZE587150331792478',
-		'geocode'  => array(
-			'lat'  => 33.39440,
-			'lng'  => -104.52262
-		)
-	),
-	array(
-		'id'       => 'NDP-NARF',
-		'name'     => 'Baron Von Savings',
-		'address'  => '398 Lucky St., Truth or Consequences, NM 87901',
-		'tracking' => 'LF002345052US',
-		'geocode'  => array(
-			'lat'  => 33.13417,
-			'lng'  => -107.23851
-		)
-	)
-);
+	);
 
-/* Setup basic view data */
-
-$view_data = array();
-
-// Setup order ID
-if (isset($_GET['order'])) {
-	$view_data['order'] = $_GET['order'];
-}
-else {
-	$view_data['order'] = rand(0, count($orders) - 1);
-}
-
-// Cache order data object
-$view_data['order_data'] = $orders[$view_data['order']];
-
-// Setup monster ID
-if (isset($_GET['monster'])) {
-	$view_data['monster'] = $_GET['monster'];
-}
-else {
-	$num_monsters = count(glob('img/monsters/*.png'));
+	/* Setup basic view data */
+	$view_data = array();
 	
-	$view_data['monster'] = base_convert($view_data['order_data']['id'], 32, 10) % $num_monsters;
-}
-
-// Setup path ID
-if (isset($_GET['path'])) {
-	$view_data['path'] = $_GET['path'];
-}
-else {
-	$num_paths = count(glob('img/paths/*.png'));
-
-	$view_data['path'] = base_convert($view_data['order_data']['name'], 32, 10) % $num_paths;
-}
+	$view_data['order_data'] = getOrderData();
+	$view_data['monster']    = getMonster();
+	$view_data['path']       = getPath();
 
 ?>
 
@@ -79,7 +52,11 @@ else {
 </head>
 
 <body>
-	<?php echo $view_data['monster']; ?><br />
-	<?php echo $view_data['path']; ?><br />
+	<div id="mapHolder">
+		<img src="img/map.png" />
+		
+		<img src="<?php echo $view_data['monster']; ?>" />
+		<img src="<?php echo $view_data['path']; ?>" />
+	</div>
 </body>
 </html>
---
+++ lib.php
@@ -0,0 +1,49 @@
+<?php
+
+	function getOrderData() {
+		global $orders;
+		
+		if (isset($_GET['order'])) {
+			$order_id = $_GET['order'];
+		}
+		else {
+			// Pick a random order
+			$order_id = rand(0, count($orders) - 1);
+		}
+
+		return $orders[$order_id];
+	}
+	
+	function getMonster() {
+		global $view_data;
+		
+		if (isset($_GET['monster'])) {
+			$monster_id = $_GET['monster'];
+		}
+		else {
+			// Generate random monster, hashing based on order number
+			$num_monsters = count(glob('img/monsters/*.png'));
+
+			$monster_id = base_convert($view_data['order_data']['id'], 32, 10) % $num_monsters;
+		}
+
+		return "img/monsters/monster{$monster_id}.png";
+	}
+
+	function getPath() {
+		global $view_data;
+
+		if (isset($_GET['path'])) {
+			$path_id = $_GET['path'];
+		}
+		else {
+			// Generate random path, hashing based on order customer's name
+			$num_paths = count(glob('img/paths/*.png'));
+
+			$path_id = base_convert($view_data['order_data']['name'], 32, 10) % $num_paths;
+		}
+
+		return "img/paths/path{$path_id}.png";
+	}
+
+?>
