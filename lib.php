9b81ad950ad31768280ad037a65ba515b4b32d9d
Phil Dokas <phil@jetless.org>
2011-07-20 12:11:08 -0400

    Perform a bit more checking of GET params, fall back to random generation in error cases.

--- lib.php
+++ lib.php
@@ -1,59 +1,86 @@
 <?php
 
+	/**
+	 * Returns an order based on GET 'order' param if available
+	 * 
+	 * @return an item from the global $orders object
+	 */
 	function getOrderData() {
 		global $orders;
 		
-		if (isset($_GET['order'])) {
+		// Pick the order requested by the GET 'order' param if it's valid
+		if (isset($_GET['order']) AND $_GET['order'] < count($orders)) {
 			$order_id = $_GET['order'];
 		}
+		// Otherwise pick a random order
 		else {
-			// Pick a random order
 			$order_id = rand(0, count($orders) - 1);
 		}
-
+		
 		return $orders[$order_id];
 	}
 	
+	/**
+	 * Returns a monster image path based on GET 'monster' param if available
+	 * 
+	 * @return string, path to a valid monster image
+	 */
 	function getMonster() {
 		global $view_data;
 		
-		if (isset($_GET['monster'])) {
+		$num_monsters = count(glob('img/monsters/*.png'));
+		
+		// Pick the monster requested by the GET 'monster' param if it's valid
+		if (isset($_GET['monster']) AND $_GET['monster'] < $num_monsters) {
 			$monster_id = $_GET['monster'];
 		}
+		// Otherwise pick random monster, hashing based on order number
 		else {
-			// Generate random monster, hashing based on order number
-			$num_monsters = count(glob('img/monsters/*.png'));
-
 			$monster_id = base_convert($view_data['order']['id'], 32, 10) % $num_monsters;
 		}
 
 		return "img/monsters/monster{$monster_id}.png";
 	}
 
+	/**
+	 * Returns a monster position ID based on GET 'monsterPos' param if available
+	 * 
+	 * @return string, monster position class (further definition in CSS)
+	 */
 	function getMonsterPos() {
 		global $view_data;
+		
+		$num_positions = 3;
 
-		if (isset($_GET['monsterPos'])) {
+		// Pick the monster position requested by the GET 'monsterPos' param if
+		// it's valid
+		if (isset($_GET['monsterPos']) AND $_GET['monsterPos'] < $num_positions) {
 			$pos_id = $_GET['monsterPos'];
 		}
+		// Otherwise select random position
 		else {
-			// Select random position
-			$pos_id = rand(0, 3);
+			$pos_id = rand(0, $num_positions);
 		}
 
 		return "monsterPosition{$pos_id}";
 	}
 
+	/**
+	 * Returns a path image path based on GET 'path' param if available
+	 * 
+	 * @return string, path to a valid path image
+	 */
 	function getPath() {
 		global $view_data;
+		
+		$num_paths = count(glob('img/paths/*.png'));
 
-		if (isset($_GET['path'])) {
+		// Pick the path requested by the GET 'path' param if it's valid
+		if (isset($_GET['path']) AND $_GET['path'] < $num_paths) {
 			$path_id = $_GET['path'];
 		}
+		// Otherwise pick random path, hashing based on order customer's name
 		else {
-			// Generate random path, hashing based on order customer's name
-			$num_paths = count(glob('img/paths/*.png'));
-
 			$path_id = base_convert($view_data['order']['name'], 32, 10) % $num_paths;
 		}
 
